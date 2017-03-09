62eb3a7928769a4986d9dbcb7c024a341e6432d5
Magdalena <magda.tworek@hotmail.com>
2017-03-09 20:21:28 +0100

    added game objects

--- gameCpp/gameCpp/gameCpp.cpp
+++ gameCpp/gameCpp/gameCpp.cpp
@@ -3,6 +3,28 @@
 
 #include "stdafx.h"
 
+struct GameObject
+{
+	Vector pos;
+
+};
+
+struct Vector
+{
+	int x, y;
+};
+
+enum ObjectType
+{
+	Player = 0,
+	Enemy = 1,
+	Treasure = 2
+};
+
+GameObject player;
+GameObject enemies[3];
+GameObject treasures[3];
+
 
 int main()
 {
