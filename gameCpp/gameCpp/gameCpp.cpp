aa69f8408094ee4483e48fe119f128ea93f7e8c8
Magdalena <magda.tworek@hotmail.com>
2017-04-06 01:05:51 +0200

    losowanie

--- gameCpp/gameCpp/gameCpp.cpp
+++ gameCpp/gameCpp/gameCpp.cpp
@@ -1,5 +1,6 @@
 // gameCpp.cpp : Defines the entry point for the console application.
-//
+//zmiana: mowic i tlumaczyc i czy dobrze
+
 
 #include "stdafx.h"
 #include <iostream>
@@ -7,11 +8,18 @@
 #include <string>
 #include <time.h>
 #include <conio.h>
+#include <vector>
+#include<windows.h>
+
+using namespace std;
+
 
 #define KEY_UP 72		// systemowe numery klawiszy
 #define KEY_DOWN 80
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
+
+#define pi 3.14
 
 using namespace std;
 
@@ -23,68 +31,114 @@ enum ObjectType
 	Treasure = 2
 };
 
-struct Vector
+struct Point
 {
 	int x, y;
 };
 
 struct GameObject
 {
-	Vector pos;
+	GameObject(int x, int y, ObjectType type)	// zmiana - po co to wlasciwie?
+	{
+		pos.x = x;
+		pos.y = y;
+		this->type = type;
+	}
+	Point pos;
 	ObjectType type;
 };
 
+vector<GameObject> gameObjects;
 
+#pragma region initialized data
 
+const int mapSize = 12;
+const int enemyNumber = 2;
+const int treasureNumber = 2;
 
-
-GameObject player;
-GameObject enemies[3];
-GameObject treasures[3];
-
-const int mapSize = 10;
 const string borderSymbol = " #";
 const string blankSymbol = "  ";
-const string playerSymbol = " A";
+const string playerSymbol = " P";
+const string enemySymbol = " !";
+const string treasureSymbol = " X";
+
+#pragma endregion
+
+
+#pragma region switch drawMap
 
 void drawMap()
 {
+	string mapa[mapSize][mapSize];
+	for (int x = 0; x < mapSize; x++)
+	{
+		for (int y = 0; y < mapSize; y++)
+		{
+			if (x == 0 || y == 0 || x == (mapSize - 1) || y == (mapSize - 1))
+				mapa[x][y] = borderSymbol;
+			else
+				mapa[x][y] = blankSymbol;
+		}
+	}
+
+	for (int w = 0; w < gameObjects.size(); w++)	// zmiana: czy jesli chce miec 2 skarby na mapie to musze zainicjowac skarb1, skarb2? czy moge da liczbe skarbow do zainicjowania, ewentualnie zrobic wektor skarbow(?)
+	{
+		switch (gameObjects[w].type)
+		{
+		case ObjectType::Enemy:
+			mapa[gameObjects[w].pos.x][gameObjects[w].pos.y] = enemySymbol;
+			break;
+		case ObjectType::Player:
+			mapa[gameObjects[w].pos.x][gameObjects[w].pos.y] = playerSymbol;
+			break;
+		case ObjectType::Treasure:
+			mapa[gameObjects[w].pos.x][gameObjects[w].pos.y] = treasureSymbol;	//zmiana
+			break;
+		}
+	}
+	
 	for (int y = 0; y < mapSize; y++)
 	{
 		for (int x = 0; x < mapSize; x++)
 		{
-			if (player.pos.x == x&& player.pos.y ==y)
-			{
-				cout << playerSymbol;
-			}
-			else if (x == 0 || y == 0 || x == (mapSize - 1) || y == (mapSize - 1))
-			{
-				cout <<borderSymbol;
-			}
-			else
-			{
-				cout << blankSymbol;
-
-			}
+			cout << mapa[x][y];
 		}
 		cout << endl;
 	}
 }
+#pragma endregion
+
 
 void initializeObjects()
-{
-	player.pos.x = 1;
-	player.pos.y = 1;
-	player.type = ObjectType::Player;	// pokazuje, ze typ gracza jest playerem
+{		//zmiana: jak sprawdzac czy wspolrzedne sa juz zajete
+	gameObjects.push_back(GameObject(1,1, ObjectType::Player));
+	
+	for (int i = 0; i < enemyNumber; i++)	//zmiana	trzeba zrobic strukture, do ktorej bede sie odwolywac, zeby sprawdzic ilosc obiektow, zeby nie robic zmiennych globalnych
+	{
+		int a, b;
+		a = rand() % (mapSize - 3) + 2;		// jak tutaj odwolac sie do punktu (Enemy.pos.x; Enemy.pos.y) zamiast tworzyc nowe zmienne i jakim cudem w drawMap tak czy tak rysuje odwolujac sie do Enemy.pos.x/y
+		b = rand() % (mapSize - 3) + 2;
+
+		gameObjects.push_back(GameObject(a, b, ObjectType::Enemy));
+		cout << a << " " << b << endl;	//zmiana: dlaczego znowu na odwrot to robi, skoro drawMap jest ok, jednak x pierwszy, dlaczego?? wtf
+		Sleep(2000);
+	}
+//zmiana - dlaczego nie moge tak zrobic:
+//		gameObjects[3].pos.x = rand() % (mapSize - 3) + 3;
+//		gameObjects[3].pos.y = rand() % (mapSize - 3) + 3;
+	gameObjects.push_back(GameObject(8/*gameObjects[3].pos.x*/, 2/*gameObjects[3].pos.y*/, ObjectType::Treasure));	// zmiana: dlaczego nie Treasure.pos.x np
+
+
 }
 
-Vector getInput()
+Point getInput()
 {
 	int input;
-	Vector output;
+	Point output;
 	output.x = 0;
 	output.y = 0;	// struktury musza byc przed uzyciem zainicjalizowane
-	input = _getch();
+	input = _getch(); // jesli nie wiem jaka wartosc ma klawisz to robie input = _getch() i wywalam input na ekran
+
 	switch (input)
 	{
 	case KEY_UP:
@@ -104,18 +158,36 @@ Vector getInput()
 	}
 	return output;
 }
+
+
 void movePlayer()
 {
-	Vector input = getInput();
-	if (player.pos.x + input.x != 0&& player.pos.x + input.x != (mapSize-1))
+	Point input = getInput();
+	if (gameObjects[0].pos.x + input.x != 0 && gameObjects[0].pos.x + input.x != (mapSize - 1))
 	{
-		player.pos.x += input.x; // rowna sie ten pierwszy dodac ten drugi
+		gameObjects[0].pos.x += input.x; // rowna sie ten pierwszy dodac ten drugi
 	}
-	if (player.pos.y + input.y != 0 && player.pos.y + input.y != (mapSize - 1))
+	if (gameObjects[0].pos.y + input.y != 0 && gameObjects[0].pos.y + input.y != (mapSize - 1))
 	{
-		player.pos.y += input.y;
+		gameObjects[0].pos.y += input.y;
+	}
+	if (gameObjects[0].pos.x == gameObjects[1].pos.x && gameObjects[0].pos.y == gameObjects[1].pos.y)
+	{
+		cout << "Padles" << endl;
+		Sleep(1000);
+		//zmiana: jak zrobic, zeby tutaj program sie konczyl
+
+	}
+	if (gameObjects[0].pos.x == gameObjects[3].pos.x && gameObjects[0].pos.y == gameObjects[3].pos.y)
+	{
+		cout << "Zebrales skarb" << endl;
+		Sleep(1000);
+	//	gameObjects[2].pos.x = -100;
+	//	gameObjects[2].pos.y = -100;  // zlo sie dzieje
+		//zmiana: jak zrobic, zeby skarb znikal, czy da sie nadac tej pozycji jakies wartosc spoza skali (nie da), np mapSize+10, zeby tego w ogole nie wyswietlalo
 	}
 }
+
 int main()
 {
 	srand(time(NULL));
@@ -130,6 +202,9 @@ int main()
 	}
 
 	system("pause");
-    return 0;
+	return 0;
 }
 
+
+
+// zeby nie stawaly na tym samym miejscu jesli pozycje takie same to losuj jeszcze raz (i--)
